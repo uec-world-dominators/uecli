2fc8fe6e38241c6546fb73c50ca5cc7e8f5aadfd
shosatojp <me@shosato.jp>
2021-10-11 01:26:56 +0900

    integrate with library commands

--- uecli/__main__.py
+++ uecli/__main__.py
@@ -1,22 +1,32 @@
+import sys
 import argparse
-from campussquare.cli import get_parser, parse_args_with_handler
-from .authenticator import UECCampusSquareAuthenticator
+import campussquare.cli
+import limedio.cli
+from .authenticator import UECCampusSquareAuthenticator, UECLibraryAuthenticator
 from . import info
 
 campussquare_commands = ['syllabus', 'grades']
+library_command = 'library'
 
 
 def main():
     parser = argparse.ArgumentParser('uecli')
     parser.add_argument('--version', '-V', action='version', version=f'{info.name} {info.version}')
-    parser.add_argument('command', choices=[*campussquare_commands])
+    parser.add_argument('command', choices=[*campussquare_commands, library_command])
     parser.add_argument('args', nargs='*')
     args, _ = parser.parse_known_args()
 
     if args.command in campussquare_commands:
         # Campus Square commands
-        parser = get_parser(authenticator=UECCampusSquareAuthenticator())
-        parse_args_with_handler(parser)
+        print(sys.argv)
+        parser = campussquare.cli.get_parser(authenticator=UECCampusSquareAuthenticator())
+        campussquare.cli.parse_args_with_handler(parser)
+    elif args.command == library_command:
+        del sys.argv[1]
+
+        # Library Command
+        parser = limedio.cli.get_parser(authenticator=UECLibraryAuthenticator())
+        limedio.cli.parse_args_with_handler(parser)
     else:
         # Other commands
         raise NotImplementedError()
--- uecli/authenticator.py
+++ uecli/authenticator.py
@@ -1,48 +1,55 @@
+import limedio.cli
+import campussquare
+from limedio.limedio import LimedioLibrary
 from uecauth.shibboleth import ShibbolethAuthenticator
 from uecauth.password import DefaultPasswordProvider, PromptingPasswordProvider
 from uecauth.mfa import AutoTOTPMFAuthCodeProvider, PromptingMFAuthCodeProvider
 
-from campussquare.cli import Authenticator
+import campussquare.cli
 from campussquare.util import get_flow_execution_key
 from campussquare import CampusSquare
+
 import os
 import sys
 
 
-class UECCampusSquareAuthenticator(Authenticator):
-    def __init__(self) -> None:
-        self.campusweb_do_url = 'https://campusweb.office.uec.ac.jp/campusweb/campussquare.do'
-        self.campusweb_url = 'https://campusweb.office.uec.ac.jp/campusweb/ssologin.do'
-        self.shibboleth_host = 'shibboleth.cc.uec.ac.jp'
-        self.shibboleth_cookies_path = os.path.expanduser('~/.uecli.cookies.lwp')
-        self.campussquare_credentials_path = os.path.expanduser('~/.uecli.campussquare.json')
+def shibboleth_login(url: str) -> None:
+    shibboleth_host = 'shibboleth.cc.uec.ac.jp'
+    shibboleth_cookies_path = os.path.expanduser('~/.uecli.cookies.lwp')
 
-
-        # Shibboleth Login
-        # setup password login
-        uec_username, uec_password = os.environ.get('UEC_USERNAME'), os.environ.get('UEC_PASSWORD')
-        if uec_username and uec_password:
-            password_provider = DefaultPasswordProvider(
-                os.environ['UEC_USERNAME'],
-                os.environ['UEC_PASSWORD']
-            )
-        else:
-            password_provider = PromptingPasswordProvider()
-
-        # setup MF Auth
-        uec_mfa_secret = os.environ.get('UEC_MFA_SECRET')
-        if uec_mfa_secret:
-            mfa_code_provider = AutoTOTPMFAuthCodeProvider(os.environ['UEC_MFA_SECRET'])
-        else:
-            mfa_code_provider = PromptingMFAuthCodeProvider()
-
-        self.shibboleth = ShibbolethAuthenticator(
-            shibboleth_host=self.shibboleth_host,
-            mfa_code_provider=mfa_code_provider,
-            password_provider=password_provider,
-            lwpcookiejar_path=self.shibboleth_cookies_path,
-            debug=False,
+    # Shibboleth Login
+    # setup password login
+    uec_username, uec_password = os.environ.get('UEC_USERNAME'), os.environ.get('UEC_PASSWORD')
+    if uec_username and uec_password:
+        password_provider = DefaultPasswordProvider(
+            os.environ['UEC_USERNAME'],
+            os.environ['UEC_PASSWORD']
         )
+    else:
+        password_provider = PromptingPasswordProvider()
+
+    # setup MF Auth
+    uec_mfa_secret = os.environ.get('UEC_MFA_SECRET')
+    if uec_mfa_secret:
+        mfa_code_provider = AutoTOTPMFAuthCodeProvider(os.environ['UEC_MFA_SECRET'])
+    else:
+        mfa_code_provider = PromptingMFAuthCodeProvider()
+
+    return ShibbolethAuthenticator(
+        shibboleth_host=shibboleth_host,
+        mfa_code_provider=mfa_code_provider,
+        password_provider=password_provider,
+        lwpcookiejar_path=shibboleth_cookies_path,
+        debug=False,
+    )
+
+
+class UECCampusSquareAuthenticator(campussquare.cli.Authenticator):
+    def __init__(self) -> None:
+        self.campusweb_url = 'https://campusweb.office.uec.ac.jp/campusweb/ssologin.do'
+        self.campusweb_do_url = 'https://campusweb.office.uec.ac.jp/campusweb/campussquare.do'
+        self.campussquare_credentials_path = os.path.expanduser('~/.uecli.campussquare.json')
+        self.shibboleth = shibboleth_login(self.campusweb_url)
 
     def login(self) -> CampusSquare:
         return CampusSquare(
@@ -67,4 +74,30 @@ class UECCampusSquareAuthenticator(Authenticator):
             self.shibboleth.get_cookies(),
             credential_path=self.campussquare_credentials_path,
             debug=False
+        )
+
+
+class UECLibraryAuthenticator(limedio.cli.Authenticator):
+    def __init__(self) -> None:
+        self.url = 'https://www.lib.uec.ac.jp/opac/user/top'
+        self.shibboleth = shibboleth_login(self.url)
+        self.prefix = 'https://www.lib.uec.ac.jp'
+
+    def login(self) -> LimedioLibrary:
+        return LimedioLibrary(
+            self.prefix,
+            cookies=self.shibboleth.get_cookies()
+        )
+
+    def refresh(self) -> LimedioLibrary:
+        print('認証情報を更新しています...', file=sys.stderr)
+        try:
+            self.shibboleth.login(self.url)
+        except Exception as e:
+            print(f'認証に失敗しました: {e}', file=sys.stderr)
+            exit(1)
+
+        return LimedioLibrary(
+            self.prefix,
+            cookies=self.shibboleth.get_cookies()
         )
